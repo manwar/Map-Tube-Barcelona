4ea3736ef13e25389cff7d24587e0ff7a4d8ad07
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-01-15 13:06:12 +0000

    - Upgraded to use Map::Tube v2.63.

--- lib/Map/Tube/Barcelona/Line/L3.pm
+++ lib/Map/Tube/Barcelona/Line/L3.pm
@@ -1,6 +1,6 @@
 package Map::Tube::Barcelona::Line::L3;
 
-$Map::Tube::Barcelona::Line::L3::VERSION   = '0.15';
+$Map::Tube::Barcelona::Line::L3::VERSION   = '0.16';
 $Map::Tube::Barcelona::Line::L3::AUTHORITY = 'cpan:MANWAR';
 
 use 5.006;
@@ -12,7 +12,7 @@ Map::Tube::Barcelona::Line::L3 - Barcelona Metro L3 Line stations.
 
 =head1 VERSION
 
-Version 0.15
+Version 0.16
 
 =encoding utf8
 
