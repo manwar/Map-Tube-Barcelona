d2a2834db90aa44172dce546d6b2873fd491af95
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-02-24 16:11:44 +0000

    - Added unit tests for Line L2, Line L5 and Line L11.

--- lib/Map/Tube/Barcelona/Line/L2.pm
+++ lib/Map/Tube/Barcelona/Line/L2.pm
@@ -1,6 +1,6 @@
 package Map::Tube::Barcelona::Line::L2;
 
-$Map::Tube::Barcelona::Line::L2::VERSION   = '0.29';
+$Map::Tube::Barcelona::Line::L2::VERSION   = '0.30';
 $Map::Tube::Barcelona::Line::L2::AUTHORITY = 'cpan:MANWAR';
 
 use 5.006;
@@ -12,7 +12,7 @@ Map::Tube::Barcelona::Line::L2 - Barcelona Metro Map: L2 Line.
 
 =head1 VERSION
 
-Version 0.29
+Version 0.30
 
 =encoding utf8
 
